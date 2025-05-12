7f7f96ac080c43862ce2055b9c5ebde023b8265d
Edh41 <eddiehaggerty41@gmail.com>
2025-05-12 10:36:56 -0600

    Put the fries in the bag

--- Assets/Scripts/ItemInteraction.cs
+++ Assets/Scripts/ItemInteraction.cs
@@ -13,7 +13,11 @@ public class ItemInteraction : MonoBehaviour
     private bool conePlaced = false;
     public float interactDistance = 3.0f;
     
+    public GameObject trashPrefab, valvePrefab, conePrefab;
+
     [SerializeField] LayerMask layerMask;
+
+
     void Update()
     {
         if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hit, interactDistance, layerMask))
@@ -23,48 +27,105 @@ public class ItemInteraction : MonoBehaviour
                 if(hit.collider.gameObject.name.Equals("Crowbar"))
                 {
                     Debug.Log("Crowbar");
+
                     crowbar = true;
+
                     Destroy(hit.collider.gameObject);
+
                     // Crowbar is now in inventory
                 }
 
                 if(hit.collider.gameObject.name.Equals("TrashBag"))
                 {
                     Debug.Log("TrashBag");
-                    // Set trash to true
-                    // Destroy trash bag
+
+                    trash = true;
+
+                    trashPrefab = hit.collider.gameObject;
+
+                    Instantiate(trashPrefab, new Vector3(10.0f,1.5f,127.9f), Quaternion.identity);
+
+                    hit.collider.gameObject.SetActive(false);
+
                     // trash bag is now in inventory
                 }
 
                 if(hit.collider.gameObject.name.Equals("SmWoodBox"))
                 {
                     Debug.Log("SmWoodBox");
-                    // Check if crowbar is true
-                    // if crowbar is true, destroy smwoodbox
-                    // set box1 to true
+
+                    if(crowbar)
+                    {
+                        Destroy(hit.collider.gameObject);
+                    }
+                    
+                    if(box1)
+                    {
+                        box2 = true;
+                    }
+
+                    else
+                    {
+                        box1 = true;
+                    }
                 }
 
                 if(hit.collider.gameObject.name.Equals("TallWoodBox"))
                 {
                     Debug.Log("TallWoodBox");
-                    // Check if crowbar is true
-                    // if crowbar is true, destroy tallwoodbox
-                    // set box2 to true
+                    
+                    if(crowbar)
+                    {
+                        Destroy(hit.collider.gameObject);
+                    }
+                    
+                    if(box1)
+                    {
+                        box2 = true;
+                    }
+
+                    else
+                    {
+                        box1 = true;
+                    }
                 }
 
                 if(hit.collider.gameObject.name.Equals("Dumpster_body"))
                 {
                     Debug.Log("Dumpster");
-                    // Check if trash is true
+                    
+                    if(trash)
+                    {
+                        trashPrefab.SetActive(true);
+
+                        Instantiate(trashPrefab, new Vector3(31.4f,17.8f,-83.3f), Quaternion.identity);
+                    }
                     // if trash is true, instantiate bag into dumpster
                     // set trash to false
                     // set trashout to true
                 }
 
+                if(hit.collider.gameObject.name.Equals("Valve"))
+                {
+                    Debug.Log("Valve");
+
+                    valve = true;
+
+                    // instantiate valve
+
+                    Destroy(hit.collider.gameObject);
+
+                    // valve is in inventory
+                }
+
                 if(hit.collider.gameObject.name.Equals("Machine"))
                 {
                     Debug.Log("Machine");
-                    // check if valve is true
+
+                    if(valve)
+                    {
+
+                    }
                     // if valve is true, instantiate valve handle onto machine
                     // set valve to false
                     // set valveplaced to true
@@ -73,15 +134,24 @@ public class ItemInteraction : MonoBehaviour
                 if(hit.collider.gameObject.name.Equals("Cone"))
                 {
                     Debug.Log("Cone");
-                    // set cone to true
-                    // destroy cone
+                    
+                    cone = true;
+                    
+                    // Instantiate cone
+                    
+                    Destroy(hit.collider.gameObject);
+
                     // cone is now in inventory
                 }
 
                 if(hit.collider.gameObject.name.Equals("Electric"))
                 {
                     Debug.Log("Electric");
-                    // check if cone is true
+                    
+                    if(cone)
+                    {
+
+                    }
                     // if cone is true, instantiate cone on ground
                     // set cone to false
                     // set coneplaced to true
@@ -90,8 +160,12 @@ public class ItemInteraction : MonoBehaviour
                 if(hit.collider.gameObject.name.Equals("ExitCriteria"))
                 {
                     Debug.Log("ExitCriteria");
-                    // check if box1, box2, valveplaced, coneplaced, and trashout are all true
-                    // if all are true, destroy exit barrier
+                    
+                    if(box1 && box2 && valvePlaced && conePlaced && trashOut)
+                    {
+                        Destroy(hit.collider.gameObject);
+                    }
+                    
                     // you can leave now
                 }
             }
